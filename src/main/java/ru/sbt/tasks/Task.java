2afafe813cdfabc469f234b312e5a1f9edbfd45a
aleksejpluhin <pk-feso@mail.ru>
2016-08-31 19:53:03 +0300

    add volatile

--- src/main/java/ru/sbt/tasks/Task.java
+++ src/main/java/ru/sbt/tasks/Task.java
@@ -7,24 +7,25 @@ public class Task<T> {
     private volatile Object result;
     private final Object lock = new Object();
     private final Callable<? extends T> call;
-    private boolean isException = false;
+    private volatile boolean isException = false;
+    private volatile boolean isFinish = false;
 
     public Task(Callable<? extends T> callable) {
         call = callable;
     }
 
     public T get() {
-        if (result != null) return objectReturn();
+        if (isFinish) return objectReturn();
         synchronized (lock) {
-            if (result != null) return objectReturn(); // for first waiting Threads
+            if (isFinish) return objectReturn(); // for first waiting Threads
             try {
                 result = call.call();
-
             } catch (Exception e) {
                 isException = true;
                 result = e;
+            } finally {
+                isFinish = true;
             }
-            System.out.println(Thread.currentThread().getName());
             return objectReturn();
         }
     }
